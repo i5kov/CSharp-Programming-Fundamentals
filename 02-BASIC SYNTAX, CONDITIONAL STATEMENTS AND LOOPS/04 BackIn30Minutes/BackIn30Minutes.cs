cf554f89d7a18549fb11b1b3cbfa717dbffeeb15
Ivan Petkov <ipetkow90@gmail.com>
2020-01-17 00:10:11 +0200

    04 Back In 30 Minutes Task Solved

---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/04 BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BackIn30Minutes
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        minutes += 30;
+
+        if (minutes > 59)
+        {
+            hours++;
+            minutes -= 60;
+        }
+        else
+        {
+            hours = 0;
+        }
+
+        if (minutes < 10)
+        {
+            Console.WriteLine($"{hours}:{minutes:00}");
+        }
+        else
+        {
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+    }
+}
