bcb35260ed38e42312c77f84b0d00806f792d046
Ivan Petkov <ipetkow90@gmail.com>
2020-01-17 15:42:49 +0200

    Task 12,13 Solved. All Lab Tasks Done

---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/12 Even Number/EvenNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class EvenNumber
+{
+    static void Main()
+    {
+        int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+        while (number % 2 != 0)
+        {
+            Console.WriteLine("Please write an even number.");
+            number = Math.Abs(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine($"The number is: {number}");
+    }
+}
