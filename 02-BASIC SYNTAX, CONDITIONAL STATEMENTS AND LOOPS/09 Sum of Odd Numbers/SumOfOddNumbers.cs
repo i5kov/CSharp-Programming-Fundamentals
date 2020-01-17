6fc9db67d7aa08d486f695f557fd238e272820bc
Ivan Petkov <ipetkow90@gmail.com>
2020-01-17 14:22:28 +0200

    Task 09,10,11 Solved

---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/09 Sum of Odd Numbers/SumOfOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SumOfOddNumbers
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= number * 2; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
