c656a1ef4ab9693ba12beb241aadb8bd9ef7bd74
Ivan Petkov <ivan.petkov@isobar.com>
2020-01-17 13:41:57 +0200

    Task 05,06 and 07 Solved

---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/05 Month Printer/MonthPrinter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class MonthPrinter
+{
+    static void Main()
+    {
+        int monthAsNumber = int.Parse(Console.ReadLine());
+
+        string[] months = {"January", "February", "March", "April",
+                           "May", "June", "July", "August",
+                           "September", "October", "November", "December"};
+
+        if (monthAsNumber > 0 && monthAsNumber <= 12)
+        {
+            Console.WriteLine(months[monthAsNumber - 1]);
+        }
+        else
+        {
+            Console.WriteLine("Error!");
+        }
+    }
+}
---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/07 Theathre Promotion/TheathrePromotion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+class TheathrePromotion
+{
+    static void Main()
+    {
+        string typeOfDay = Console.ReadLine().ToLower();
+        int ageOfPerson = int.Parse(Console.ReadLine());
+
+        int price = 0;
+
+        switch (typeOfDay)
+        {
+            case "weekday":
+                if ((ageOfPerson >= 0 && ageOfPerson <= 18) || (ageOfPerson > 64 && ageOfPerson <= 122))
+                {
+                    price = 12;
+                }
+                else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                {
+                    price = 18;
+                }
+                break;
+
+            case "weekend":
+                if ((ageOfPerson >= 0 && ageOfPerson <= 18) || (ageOfPerson > 64 && ageOfPerson <= 122))
+                {
+                    price = 15;
+                }
+                else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                {
+                    price = 20;
+                }
+                break;
+
+            case "holiday":
+                if (ageOfPerson >= 0 && ageOfPerson <= 18)
+                {
+                    price = 5;
+                }
+                else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                {
+                    price = 12;
+                }
+                else if (ageOfPerson > 64 && ageOfPerson <= 122)
+                {
+                    price = 10;
+                }
+                break;
+        }
+
+        if (price <= 0)
+        {
+            Console.WriteLine("Error!");
+        }
+        else
+        {
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
