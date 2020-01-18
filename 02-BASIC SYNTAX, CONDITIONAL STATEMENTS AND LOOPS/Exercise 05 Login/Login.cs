bc78b7a2ea2cedae2c8f744bca9fcddc22619fe6
Ivan Petkov <ipetkow90@gmail.com>
2020-01-18 13:11:13 +0200

    Exercise Task 05,06 Solved

---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Exercise 05 Login/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class Login
+{
+    static void Main()
+    {
+        string username = Console.ReadLine();
+        string password = Console.ReadLine();
+
+        string realPassword = "";
+        for (int i = username.Length - 1; i >= 0; i--)
+        {
+            realPassword += username[i];
+        }
+
+        int tries = 1;
+        while(true)
+        {
+            if (realPassword == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+                break;
+            }
+            else
+            {
+                if (tries > 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                password = Console.ReadLine();
+                tries++;
+            }
+        }
+    }
+}
---
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Exercise 06 Strong Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int inputNumber = number;
+
+        int sum = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+
+            int factorial = 1;
+            
+            for (int i = 1; i <= digit; i++)
+            {
+                factorial *= i;
+            }
+
+            sum += factorial;
+            number /= 10;
+        }
+
+        if (sum == inputNumber)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+            
+    }
+}
