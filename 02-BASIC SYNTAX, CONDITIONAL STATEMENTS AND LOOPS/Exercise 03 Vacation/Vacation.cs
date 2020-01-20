ffdbee3a0d8b66dc1c2b8709d6522fffba25f438
Ivan Petkov <ipetkow90@gmail.com>
2020-01-20 21:55:50 +0200

    Task 03 Refactoring

--- 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Exercise 03 Vacation/Vacation.cs
+++ 02-BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Exercise 03 Vacation/Vacation.cs
@@ -6,64 +6,64 @@ class Vacation
     static void Main()
     {
         int people = int.Parse(Console.ReadLine());
-        string group = Console.ReadLine();
-        string day = Console.ReadLine();
+        string group = Console.ReadLine().ToLower();
+        string day = Console.ReadLine().ToLower();
         decimal price = 0.00m;
 
-        if (group == "Students")
+        if (group == "students")
         {
-            if (day == "Friday")
+            if (day == "friday")
             {
                 price = 8.45m;
             }
-            else if (day == "Saturday")
+            else if (day == "saturday")
             {
                 price = 9.80m;
             }
-            else if (day == "Sunday")
+            else if (day == "sunday")
             {
                 price = 10.46m;
             }
         }
-        else if (group == "Business")
+        else if (group == "business")
         {
-            if (day == "Friday")
+            if (day == "friday")
             {
                 price = 10.90m;
             }
-            else if (day == "Saturday")
+            else if (day == "saturday")
             {
                 price = 15.60m;
             }
-            else if (day == "Sunday")
+            else if (day == "sunday")
             {
                 price = 16;
             }
         }
-        else if (group == "Regular")
+        else if (group == "regular")
         {
-            if (day == "Friday")
+            if (day == "friday")
             {
                 price = 15;
             }
-            else if (day == "Saturday")
+            else if (day == "saturday")
             {
                 price = 20;
             }
-            else if (day == "Sunday")
+            else if (day == "sunday")
             {
                 price = 22.50m;
             }
         }
-        if (people >= 30 && group == "Students")
+        if (people >= 30 && group == "students")
         {
             price *= 0.85m;
         }
-        else if (people >= 100 && group == "Business")
+        else if (people >= 100 && group == "business")
         {
             people -= 10;
         }
-        else if ((people >= 10 && people <= 20) && group == "Regular")
+        else if ((people >= 10 && people <= 20) && group == "regular")
         {
             price *= 0.95m;
         }
