9229309e276a07e60fda669d2575bdcc097cba46
Ivan Petkov <ipetkow90@gmail.com>
2020-01-21 23:49:20 +0200

    Task 06,07 solved

---
+++ 03-DATA TYPES AND VARIABLES/06 Reversed Chars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string first = Console.ReadLine();
+        string second = Console.ReadLine();
+        string third = Console.ReadLine();
+        string result = string.Format($"{first} {second} {third}");
+
+        for (int i = result.Length - 1; i >= 0; i--)
+        {
+            Console.Write(result[i]);
+        }
+    }
+}
---
+++ 03-DATA TYPES AND VARIABLES/07 Concat Names/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name1 = Console.ReadLine();
+        string name2 = Console.ReadLine();
+        string delimeter = Console.ReadLine();
+
+        Console.WriteLine($"{name1}{delimeter}{name2}");
+    }
+}
