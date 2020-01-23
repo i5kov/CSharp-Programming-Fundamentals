55e801aafd3509bdf12aa56a4bbf1ac1773794ac
Ivan Petkov <ipetkow90@gmail.com>
2020-01-23 15:58:03 +0200

    Arrays Lab Task Solved

---
+++ 04-ARRAYS/02 Print Numbers in Reverse Order/NumbersInReverseOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class NumbersInReverseOrder
+{
+    static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        int[] arr = new int[lines];
+
+        for (int i = 0; i < lines; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
---
+++ 04-ARRAYS/07 Equal Arrays/EqualArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class EqualArrays
+{
+    static void Main(string[] args)
+    {
+        int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int sum = 0;
+        bool isEqual = true;
+
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            if (firstArray[i] != secondArray[i])
+            {
+                isEqual = false;
+                Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                break;
+            }
+            sum += firstArray[i];
+        }
+
+        if (isEqual)
+        {
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
