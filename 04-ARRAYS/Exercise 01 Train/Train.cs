83a4ca2d45db5897120ba5203f2b215964486b95
Ivan Petkov <ipetkow90@gmail.com>
2020-01-24 10:21:47 +0200

    Exercise Task 01-03 Solved

---
+++ 04-ARRAYS/Exercise 01 Train/Train.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Train
+{
+    static void Main(string[] args)
+    {
+        int wagons = int.Parse(Console.ReadLine());
+        int[] train = new int[wagons];
+        int totalPeople = 0;
+
+        for (int i = 0; i < wagons; i++)
+        {
+            train[i] = int.Parse(Console.ReadLine());
+            totalPeople += train[i];
+        }
+
+        for (int i = 0; i < train.Length; i++)
+        {
+            Console.Write(train[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(totalPeople);
+    }
+}
