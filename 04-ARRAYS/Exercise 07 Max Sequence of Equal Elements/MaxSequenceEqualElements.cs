4c0cd30e3b4a03ad3627d6669e7834caf3e3760c
Ivan Petkov <ipetkow90@gmail.com>
2020-01-24 12:27:17 +0200

    Tasks 04,05,08 Solved

---
+++ 04-ARRAYS/Exercise 07 Max Sequence of Equal Elements/MaxSequenceEqualElements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class MaxSequenceEqualElements
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int longestSequence = 0;
+
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+                
+        }
+    }
+}
