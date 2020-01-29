2de32a003885e87eb6ad53450ad85a6ebc0a654d
Ivan Petkov <ipetkow90@gmail.com>
2020-01-29 21:22:18 +0200

    task 08 and 11 solved

---
+++ 03-DATA TYPES AND VARIABLES/Exercise 11 Snowballs/Snowballs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+class Snowballs
+{
+    static void Main()
+    {
+        int snowballs = int.Parse(Console.ReadLine());
+
+        BigInteger maxValue = 0;
+        string bestResult = "";
+
+        for (int i = 0; i < snowballs; i++)
+        {
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+            if (snowballValue >= maxValue)
+            {
+                maxValue = snowballValue;
+                string result = ($"{snowballSnow} : {snowballTime} = {maxValue} ({snowballQuality})");
+                bestResult = result;
+            }
+        }
+        Console.WriteLine(bestResult);
+    }
+}
