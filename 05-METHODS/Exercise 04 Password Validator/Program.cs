c7f7efdbb05925885e627dc5d76f6c9236a11e94
Ivan Petkov <ipetkow90@gmail.com>
2020-02-07 12:05:00 +0200

    Fixing the code of the task 100/100 points in judge

--- 05-METHODS/Exercise 04 Password Validator/Program.cs
+++ 05-METHODS/Exercise 04 Password Validator/Program.cs
@@ -1,59 +1,63 @@
 ﻿using System;
 
+
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         string password = Console.ReadLine();
+        bool passwordLenght = PasswordLenght(password);
+        bool passwordLetterAndDigits = PasswordLetterAndDigits(password);
+        bool passwordDigitCounter = PasswordDigitCounter(password);
 
-        if (IsValidLength(password) && IsHasAtleast2Digit(password) && IsHasAtleast2Digit(password))
+        if (passwordLenght && passwordLetterAndDigits && passwordDigitCounter)
         {
             Console.WriteLine("Password is valid");
         }
 
-        if (!IsValidLength(password))
+        if (!passwordLenght)
         {
             Console.WriteLine("Password must be between 6 and 10 characters");
         }
-        if (!IsHasOnlyLettersAndDigit(password))
+        if (!passwordLetterAndDigits)
         {
             Console.WriteLine("Password must consist only of letters and digits");
         }
-        if (!IsHasAtleast2Digit(password))
+        if (!passwordDigitCounter)
         {
             Console.WriteLine("Password must have at least 2 digits");
         }
-
     }
 
-    private static bool IsValidLength(string password)
+    private static bool PasswordLenght(string password)
     {
         return password.Length >= 6 && password.Length <= 10;
     }
 
-    private static bool IsHasAtleast2Digit(string password)
+    private static bool PasswordDigitCounter(string password)
     {
-        int digit = 0;
+        int counter = 0;
         for (int i = 0; i < password.Length; i++)
         {
-            if (Char.IsDigit(password[i]))
+            if (char.IsDigit(password[i]))
             {
-                digit++;
+                counter++;
             }
         }
 
-        return digit >= 2;
+        return counter >= 2;
     }
 
-    private static bool IsHasOnlyLettersAndDigit(string password)
+    private static bool PasswordLetterAndDigits(string password)
     {
+        bool IsLetterAndDigitOnly = true;
         for (int i = 0; i < password.Length; i++)
         {
-            if (!Char.IsLetterOrDigit(password[i]))
+            if (!(char.IsLetterOrDigit(password[i])))
             {
-                return false;
+                IsLetterAndDigitOnly = false;
             }
         }
-        return true;
+        return IsLetterAndDigitOnly;
     }
 }
