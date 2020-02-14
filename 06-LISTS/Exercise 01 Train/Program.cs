5ddad32dc0ca7d43e2c6118b558aabcbc106db12
Ivan Petkov <ipetkow90@gmail.com>
2020-02-14 15:01:47 +0200

    Starting Tasks From Exercise Section

---
+++ 06-LISTS/Exercise 01 Train/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            string[] command = Console.ReadLine().Split();
+            if (command.Length == 1)
+            {
+                if (command[0] == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    int morePassengers = int.Parse(command[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + morePassengers <= maxCapacityOfWagon)
+                        {
+                            wagons[i] += morePassengers;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                int morePassengers = int.Parse(command[1]);
+                wagons.Add(morePassengers);
+            }  
+        }
+        Console.WriteLine(string.Join(" ", wagons));
+    }
+}
