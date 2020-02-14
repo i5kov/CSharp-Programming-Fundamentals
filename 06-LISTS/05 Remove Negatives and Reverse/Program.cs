ad90538a9ac05e6a92b2bf4ef3fe126c5f399790
Ivan Petkov <ipetkow90@gmail.com>
2020-02-14 14:30:20 +0200

    Lab Tasks Done

---
+++ 06-LISTS/05 Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> positiveNumbers = new List<int>();   
+
+        foreach (int currentNumber in numbers)
+        {
+            if (currentNumber >= 0)
+            {
+                positiveNumbers.Add(currentNumber);
+            }
+        }
+
+        if (positiveNumbers.Count > 0)
+        {
+            positiveNumbers.Reverse();
+            Console.WriteLine(string.Join(" ", positiveNumbers));
+        }
+        else
+        {
+            Console.WriteLine("empty");
+        }       
+    }
+}
---
+++ 06-LISTS/06 List Manipulation Basics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        string[] command = Console.ReadLine().ToLower().Split();
+
+        while (command[0] != "end")
+        {
+            int item = int.Parse(command[1]);
+            switch (command[0])
+            {
+                case "add":
+                    numbers.Add(item); 
+                    break;
+                case "remove":
+                    numbers.Remove(item); 
+                    break;
+                case "removeat":
+                    numbers.RemoveAt(item); 
+                    break;
+                case "insert":
+                    int number = int.Parse(command[1]);
+                    int index = int.Parse(command[2]);
+                    numbers.Insert(index, number);
+                    break;
+            }
+            command = Console.ReadLine().ToLower().Split();           
+        }
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
