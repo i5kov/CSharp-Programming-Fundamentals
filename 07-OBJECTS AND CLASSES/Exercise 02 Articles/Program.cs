47f0927928e8f5b754ea82ede5d9284d14b87b18
Ivan Petkov <ipetkow90@gmail.com>
2020-02-21 17:16:19 +0200

    Exercise Task 01 and 02 Solved

---
+++ 07-OBJECTS AND CLASSES/Exercise 02 Articles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] article = Console.ReadLine().Split(", ");
+        int numberOfCommands = int.Parse(Console.ReadLine());
+        Article myArticle = new Article(article[0], article[1], article[2]);
+
+        for (int i = 0; i < 3; i++)
+        {
+
+            string[] command = Console.ReadLine().Split(": ");
+            string operation = command[0].ToLower();
+            string text = command[1];
+
+            switch (operation)
+            {
+                case "edit":
+                    myArticle.Edit(text);
+                    break;
+                case "changeauthor":
+                    myArticle.ChangeAuthor(text);
+                    break;
+                case "rename":
+                    myArticle.Rename(text);
+                    break;
+            }
+
+        }
+        Console.WriteLine(myArticle);
+
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string content)
+        {
+            this.Content = content;
+        }
+
+        public void ChangeAuthor(string author)
+        {
+            this.Author = author;
+        }
+
+        public void Rename(string title)
+        {
+            this.Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
