b38407bbcf9c2c72f6f5e2176aab88b316a1bdb5
Ivan Petkov <ipetkow90@gmail.com>
2020-02-21 11:58:00 +0200

    Lab Tasks 01-03 Solved

---
+++ 07-OBJECTS AND CLASSES/02 Randomize Words/RandomizeWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class RandomizeWords
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] words = input.Split();
+
+        Random random = new Random();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            int index = random.Next(0, words.Length - 1);
+            string temp = words[index];
+            words[index] = words[i];
+            words[i] = temp;
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine, words));
+    }
+}
