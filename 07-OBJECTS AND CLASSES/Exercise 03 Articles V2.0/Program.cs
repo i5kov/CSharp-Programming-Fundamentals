68275140c7ee66c638838387240cd888909dc403
Ivan Petkov <ipetkow90@gmail.com>
2020-02-22 20:56:31 +0200

    Exercise Task 03 and 04 Solved

---
+++ 07-OBJECTS AND CLASSES/Exercise 03 Articles V2.0/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int numberOfArticles = int.Parse(Console.ReadLine());
+        List<Article> articles = new List<Article>();
+
+        for (int i = 0; i < numberOfArticles; i++)
+        {
+            string[] article = Console.ReadLine().Split(", ");
+            string articleTitle = article[0];
+            string articleContent = article[1];
+            string articleAuthor = article[2];
+
+            articles.Add(new Article(articleTitle, articleContent, articleAuthor));
+        }
+
+        string filterBy = Console.ReadLine().ToLower();
+
+        switch (filterBy)
+        {
+            case "title":
+                articles.OrderBy(x => x.Title).ToList().ForEach(x => Console.WriteLine(x));
+                break;
+            case "content":
+                articles.OrderBy(x => x.Content).ToList().ForEach(x => Console.WriteLine(x));
+                break;
+            case "author":
+                articles.OrderBy(x => x.Author).ToList().ForEach(x => Console.WriteLine(x));
+                break;
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ 07-OBJECTS AND CLASSES/Exercise 04 Students/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int countOfStudents = int.Parse(Console.ReadLine());
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < countOfStudents; i++)
+        {
+            string[] studentInfo = Console.ReadLine().Split();
+            string studentFirstName = studentInfo[0];
+            string studentLastName = studentInfo[1];
+            double studentGrade = double.Parse(studentInfo[2]);
+
+            students.Add(new Student(studentFirstName, studentLastName, studentGrade));
+        }
+
+        students
+            .OrderByDescending(x => x.Grade)
+            .ToList()
+            .ForEach(x =>
+            {
+                Console.WriteLine($"{x.FirstName} {x.LastName}: {x.Grade:f2}");
+            });
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+}
