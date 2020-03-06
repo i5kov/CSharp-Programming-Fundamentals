f63c7aa77bdb1d05f90d3ebbd75e08772177caea
Ivan Petkov <ipetkow90@gmail.com>
2020-03-06 23:45:12 +0200

    Exercise Task 05 Solved

---
+++ 10-ASSOCIATIVE ARRAYS/Exercise 05 SoftUni Parking/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+
+        Dictionary<string, string> registered = new Dictionary<string, string>();
+
+        for (int i = 0; i < lines; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            string command = input[0];
+            string username = input[1];
+
+            if (command == "register")
+            {
+                
+                string licensePlateNumber = input[2];
+
+                if (!registered.ContainsKey(username))
+                {
+                    registered.Add(username, licensePlateNumber);
+                    Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                }
+            }
+            else
+            {
+                if (!registered.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: user {username} not found");
+                }
+                else
+                {
+                    registered.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                }
+            }
+        }
+
+        foreach (var item in registered)
+        {
+            Console.WriteLine($"{item.Key} => {item.Value}");
+        }
+    }
+}
