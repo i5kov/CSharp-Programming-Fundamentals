2e21a5f0ce1f0521669315ab5f6dfabd5d52d7e1
Ivan Petkov <ipetkow90@gmail.com>
2020-03-06 14:37:19 +0200

    Lab Tasks 04-05 Solved Lab Tasks DONE!

---
+++ 10-ASSOCIATIVE ARRAYS/04 Largest 3 Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        numbers
+            .OrderByDescending(x => x)
+            .Take(3)
+            .ToList()
+            .ForEach(x => Console.Write(x + " "));
+    }
+}
---
+++ 10-ASSOCIATIVE ARRAYS/05 Word Filter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] words = Console.ReadLine().Split();
+
+        words
+            .Where(x => x.Length % 2 == 0)
+            .ToList()
+            .ForEach(x => Console.WriteLine(x));
+    }
+}
