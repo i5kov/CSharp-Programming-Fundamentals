1f35825a7d5627a7739201046bca649c5cb911ff
Ivan Petkov <ipetkow90@gmail.com>
2020-03-17 16:27:55 +0200

    Tasks 01 and 02 Solved

---
+++ 11-TEXT PROCESSING/01 Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+
+        while (true)
+        {
+            string word = Console.ReadLine();
+            string reverseWord = null;
+
+            if (word == "end")
+            {
+                break;
+            }
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseWord += word[i];
+            }
+
+            Console.WriteLine($"{word} = {reverseWord}");
+        }
+    }
+}
---
+++ 11-TEXT PROCESSING/02 Repeat Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] words = Console.ReadLine().Split();
+        StringBuilder result = new StringBuilder();
+
+        foreach (string currentWord in words)
+        {
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                result.Append(currentWord);
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
