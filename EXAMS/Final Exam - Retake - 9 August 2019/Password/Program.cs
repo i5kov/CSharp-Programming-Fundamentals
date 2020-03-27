278852938b8b7c949645ee1a84bb36f3c03021fc
Ivan Petkov <ipetkow90@gmail.com>
2020-03-27 22:38:57 +0200

    Exam Solved

---
+++ EXAMS/Final Exam - Retake - 9 August 2019/Password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Regex regex = new Regex(@"(\w+|\W+)>([0-9]{3})\|([a-z]{3})\|([A-Z]{3})\|([^<>]{3})<\1$");
+
+            for (int i = 0; i < n; i++)
+            {
+                string password = Console.ReadLine();
+                Match match = regex.Match(password);
+
+                if (match.Success)
+                {
+                    Console.WriteLine($"Password: {match.Groups[2]}{match.Groups[3]}{match.Groups[4]}{match.Groups[5]}");
+                }
+                else
+                {
+                    Console.WriteLine("Try another password!");
+                }
+            }
+        }
+    }
+}
---
+++ EXAMS/Final Exam - Retake - 9 August 2019/Username/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username = Console.ReadLine();
+        string command = Console.ReadLine();
+
+        while (command != "Sign up")
+        {
+            string[] split = command.Split();
+            if (command.Contains("Case"))
+            {
+                if (split[1] == "lower")
+                {
+                    username = username.ToLower();
+                }
+                else
+                {
+                    username = username.ToUpper();
+                }
+                Console.WriteLine(username);
+            }
+            if (command.Contains("Reverse"))
+            {
+                int startIndex = int.Parse(split[1]);
+                int endIndex = int.Parse(split[2]);
+                if ( (startIndex > 0 && startIndex <= username.Length) && (endIndex > 0 && endIndex <= username.Length))
+                {
+                    string reversed = string.Join("", string.Join("", username.Reverse()).Substring(startIndex, endIndex).Reverse());
+                    Console.WriteLine(reversed);
+                }
+            }
+            if (command.Contains("Cut"))
+            {
+                string substring = split[1];
+                if (username.Contains(substring))
+                {
+                    username = username.Remove(username.IndexOf(substring), substring.Length);
+                    Console.WriteLine(username);
+                }
+                else
+                {
+                    Console.WriteLine($"The word {username} doesn't contain {substring}.");
+                }
+            }
+            if (command.Contains("Replace"))
+            {
+                username = username.Replace(split[1], "*");
+                Console.WriteLine(username);
+            }
+            if (command.Contains("Check"))
+            {
+                if (username.Contains(split[1]))
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine($"Your username must contain {split[1]}.");
+                }
+            }
+
+
+            command = Console.ReadLine();
+        }
+        
+    }
+}
