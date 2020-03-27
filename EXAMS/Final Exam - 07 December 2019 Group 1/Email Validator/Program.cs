e77026445ff2e899a73196bff89e7ca5bd886701
Ivan Petkov <ipetkow90@gmail.com>
2020-03-27 23:28:54 +0200

    Exam Solved

---
+++ EXAMS/Final Exam - 07 December 2019 Group 1/Email Validator/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string email = Console.ReadLine();
+        string command = Console.ReadLine();
+
+        while (command != "Complete")
+        {
+            string[] split = command.Split();
+            if (command.Contains("Make Upper"))
+            {
+                email = email.ToUpper();
+                Console.WriteLine(email);
+            }
+            if (command.Contains("Make Lower"))
+            {
+                email = email.ToLower();
+                Console.WriteLine(email);
+            }
+            if (command.Contains("GetDomain"))
+            {
+                string domain = email.Substring(email.Length - int.Parse(split[1]));
+                Console.WriteLine(domain);
+
+            }
+            if (command.Contains("GetUsername"))
+            {
+                if (!email.Contains("@"))
+                {
+                    Console.WriteLine($"The email {email} doesn't contain the @ symbol.");
+                } 
+                else
+                {
+                    int index = email.IndexOf("@");
+                    string username = email.Substring(0, index);
+                    Console.WriteLine(username);
+                }
+            }
+            if (command.Contains("Replace"))
+            {
+                email = email.Replace(split[1], "-");
+                Console.WriteLine(email);
+            }
+            if (command.Contains("Encrypt"))
+            {
+                for (int i = 0; i < email.Length; i++)
+                {
+                    Console.Write($"{(int)email[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+            command = Console.ReadLine();
+        }
+    }
+}
---
+++ EXAMS/Final Exam - 07 December 2019 Group 1/Inbox Manager/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+
+        Dictionary<string, List<string>> users = new Dictionary<string, List<string>>();
+
+        while (command != "Statistics")
+        {
+            string[] split = command.Split("->");
+            if (command.Contains("Add"))
+            {
+                if (!users.ContainsKey(split[1]))
+                {
+                    users.Add(split[1], new List<string>());
+                }
+                else
+                {
+                    Console.WriteLine($"{split[1]} is already registered");
+                }
+            }
+            if (command.Contains("Send"))
+            {
+                users[split[1]].Add(split[2]);
+            }
+            if (command.Contains("Delete"))
+            {
+                if (users.ContainsKey(split[1]))
+                {
+                    users.Remove(split[1]);
+                }
+                else
+                {
+                    Console.WriteLine($"{split[1]} not found!");
+                }
+            }
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Users count: {users.Count}");
+
+        foreach (var item in users.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key).ToList())
+        {
+            Console.WriteLine(item.Key);
+            foreach (var mail in item.Value)
+            {
+                Console.WriteLine($" - {mail}");
+            }
+        }
+    }
+}
