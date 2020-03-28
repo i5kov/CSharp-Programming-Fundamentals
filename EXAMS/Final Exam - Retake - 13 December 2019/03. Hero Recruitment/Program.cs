3d47be3686b2105f311b81f0a9ee5276807de330
Ivan Petkov <ipetkow90@gmail.com>
2020-03-28 18:57:08 +0200

    Exam Solved

---
+++ EXAMS/Final Exam - Retake - 13 December 2019/03. Hero Recruitment/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Hero_Recruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroes = new Dictionary<string, List<string>>();
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] split = command.Split();
+                string heroName = split[1];
+
+                if (command.Contains("Enroll"))
+                {
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        heroes.Add(heroName, new List<string>());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{heroName} is already enrolled.");
+                    }
+                }
+                if (command.Contains("Learn"))
+                {
+                    string spellName = split[2];
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        Console.WriteLine($"{heroName} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (heroes[heroName].Contains(spellName))
+                        {
+                            Console.WriteLine($"{heroName} has already learnt {spellName}.");
+                        }
+                        else
+                        {
+                            heroes[heroName].Add(spellName);
+                        }
+                    }
+                }
+                if (command.Contains("Unlearn"))
+                {
+                    string spellName = split[2];
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        Console.WriteLine($"{heroName} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (!heroes[heroName].Contains(spellName))
+                        {
+                            Console.WriteLine($"{heroName} doesn't know {spellName}.");
+                        }
+                        else
+                        {
+                            heroes[heroName].Remove(spellName);
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            heroes = heroes
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine("Heroes:");
+            foreach (var hero in heroes)
+            {
+                Console.WriteLine($"== {hero.Key}: {string.Join(", ", hero.Value)}");
+            }
+        }
+    }
+}
