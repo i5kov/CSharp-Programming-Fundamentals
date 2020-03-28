104e55b7b3a77c9102affd6b80daf98287a4f332
Ivan Petkov <ipetkow90@gmail.com>
2020-03-28 10:03:15 +0200

    Exam Solved

---
+++ EXAMS/Final Exam - Retake - 13 December 2019/Boss Rush/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Regex regex = new Regex(@"\|([A-Z]{4,})\|:#([A-Za-z]+ [A-Za-z]+)#");
+
+        for (int i = 0; i < n; i++)
+        {
+            string nameTitle = Console.ReadLine();
+            Match match = regex.Match(nameTitle);
+
+            if (match.Success)
+            {
+                Console.WriteLine($"{match.Groups[1]}, The {match.Groups[2]}");
+                Console.WriteLine($">> Strength: {match.Groups[1].Length}");
+                Console.WriteLine($">> Armour: {match.Groups[2].Length}");
+            }
+            else
+            {
+                Console.WriteLine("Access denied!");
+            }
+        }
+    }
+}
---
+++ EXAMS/Final Exam - Retake - 13 December 2019/Warrior’s Quest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string skill = Console.ReadLine();
+        string command = Console.ReadLine();
+
+        while (command != "For Azeroth")
+        {
+            string[] split = command.Split();
+            if (command.Contains("GladiatorStance"))
+            {
+                skill = skill.ToUpper();
+                Console.WriteLine(skill);
+            }
+            else if (command.Contains("DefensiveStance"))
+            {
+                skill = skill.ToLower();
+                Console.WriteLine(skill);
+            }
+            else if (command.Contains("Dispel"))
+            {
+                int index = int.Parse(split[1]);
+                char c = char.Parse(split[2]);
+                if (index >= 0 && index <= skill.Length)
+                {
+                    skill = skill.Replace(skill[index], c);
+                    Console.WriteLine("Success!");
+                }
+                else
+                {
+                    Console.WriteLine("Dispel too weak.");
+                }
+            }
+            else if (command.Contains("Target Change"))
+            {
+                skill = skill.Replace(split[2], split[3]);
+                Console.WriteLine(skill);
+            }
+            else if (command.Contains("Target Remove"))
+            {
+                skill = skill.Remove(skill.IndexOf(split[2]), split[2].Length);
+                Console.WriteLine(skill);
+            }
+            else
+            {
+                Console.WriteLine("Command doesn't exist!");
+            }
+            command = Console.ReadLine();
+        }
+    }
+}
