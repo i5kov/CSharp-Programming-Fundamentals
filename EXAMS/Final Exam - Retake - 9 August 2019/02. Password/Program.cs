7937db91e13fc3333ed3e08d5d7a1bc66b3378da
Ivan Petkov <ipetkow90@gmail.com>
2020-04-01 10:46:30 +0300

    Exam Solved

--- EXAMS/Final Exam - Retake - 9 August 2019/02. Password/Program.cs
+++ EXAMS/Final Exam - Retake - 9 August 2019/02. Password/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace Password
+namespace _02._Password
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Regex regex = new Regex(@"^([\w|\W]+)>([0-9]{3})\|([a-z]{3})\|([A-Z]{3})\|([^<>]{3})<\1$");
             int n = int.Parse(Console.ReadLine());
-
-            Regex regex = new Regex(@"(\w+|\W+)>([0-9]{3})\|([a-z]{3})\|([A-Z]{3})\|([^<>]{3})<\1$");
 
             for (int i = 0; i < n; i++)
             {
@@ -18,7 +17,11 @@ namespace Password
 
                 if (match.Success)
                 {
-                    Console.WriteLine($"Password: {match.Groups[2]}{match.Groups[3]}{match.Groups[4]}{match.Groups[5]}");
+                    string group1 = match.Groups[2].Value;
+                    string group2 = match.Groups[3].Value;
+                    string group3 = match.Groups[4].Value;
+                    string group4 = match.Groups[5].Value;
+                    Console.WriteLine($"Password: {group1+group2+group3+group4}");
                 }
                 else
                 {
