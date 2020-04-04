bc26a0fa90dde9e88c8655b54069b56e5487f13e
Ivan Petkov <ipetkow90@gmail.com>
2020-04-04 13:06:25 +0300

    Lecture Completed - All Tasks Solved and Materials Uploaded

---
+++ EXAMS/Final Exam - Preparation - 24 July 2019/Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Regex regex = new Regex(@"");
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            Match match = regex.Match(input);
+
+            if (match.Success)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ EXAMS/Mid Exam - 29 February 2020 Group 2/02. Shopping List/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> result = Console.ReadLine().Split("!").ToList();
+            string command = Console.ReadLine();
+
+            while (command != "Go Shopping!")
+            {
+                string[] split = command.Split();
+                if (command.Contains("Urgent"))
+                {
+                    if (!result.Contains(split[1]))
+                    {
+                        result.Insert(0, split[1]);
+                    }
+                } 
+                if (command.Contains("Unnecessary"))
+                {
+                    if (result.Contains(split[1]))
+                    {
+                        result.Remove(split[1]);
+                    }
+                }
+                if (command.Contains("Correct"))
+                {
+                    if (result.Contains(split[1]))
+                    {
+                        int index = result.IndexOf(split[1]);
+                        result.Remove(split[1]);
+                        result.Insert(index, split[2]);
+                    }
+                }
+                if (command.Contains("Rearrange"))
+                {
+                    if (result.Contains(split[1]))
+                    {
+                        result.Remove(split[1]);
+                        result.Add(split[1]);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
