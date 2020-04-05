10030de6c8fa59bdc1c05ba6509bea61243d26a6
Ivan Petkov <ipetkow90@gmail.com>
2020-04-05 15:40:29 +0300

    Exam Solved

---
+++ PREVIOUS EXAMS/Mix Exams/Mid Exam - 29 February 2020 Group 2/01. National Court/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int employeeOne = int.Parse(Console.ReadLine());
+            int employeeTwo = int.Parse(Console.ReadLine());
+            int employeeThree = int.Parse(Console.ReadLine());
+            int total = employeeOne + employeeTwo + employeeThree;
+            int people = int.Parse(Console.ReadLine());
+            int peopleLeft = people;
+
+            int hours = 0;
+
+            for (int i = 1; i < people; i++)
+            {
+                if (peopleLeft <= 0)
+                {
+                    break;
+                }
+
+                if (i % 4 == 0)
+                {
+                }
+                else
+                {
+                    peopleLeft -= total;
+                }
+
+                hours++;
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
